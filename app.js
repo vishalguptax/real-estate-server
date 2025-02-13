c9e244284db6395919e1d19f76710c813807f876
Ranjna-21 <ranjna262@gmail.com>
2025-02-13 23:41:07 +0530

    correcting mistakes

---
+++ app.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import authRoute from "./routes/auth.route.js";
+import { errorHandler } from "./helpers/errorHandler.js";
+import userRoute from './routes/userRoute.js';
+import { chatRouter } from "./routes/chatRoute.js";
+import { messageRouter } from "./routes/messageRoute.js";
+import router from "./routes/auth.route.js";
+dotenv.config({
+    path: "./.env",
+})
+
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/chats",chatRouter);
+app.use("/api/message",messageRouter);
+app.use("/api/user",router);
+
+app.use(errorHandler);
+//const PORT = process.env.APP_PORT||5000;
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log(`Server is working on port: ${PORT}`)
+})
